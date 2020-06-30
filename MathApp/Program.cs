2664d0b9ea9523fc48526dcecc8f00fc31fe9836
realtommypoole <65101524+realtommypoole@users.noreply.github.com>
2020-06-30 15:52:32 -0700

    small stuff

---
+++ MathApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hi there, enter a one or two digit number and hit enter.");
+            string answer = Console.ReadLine();
+            int b = Convert.ToInt32(answer);
+            //int c = b;
+            int d = b*50;
+            Console.WriteLine(d);
+
+            Console.WriteLine("Enter another one or two digit number and hit enter.");
+            string ansTwo = Console.ReadLine();
+            int e = Convert.ToInt32(ansTwo);
+            int f = e + 25;
+            Console.WriteLine(f);
+
+            Console.WriteLine("Enter a two digit number and hit enter.");
+            string ansThree = Console.ReadLine();
+            int g = Convert.ToInt32(ansThree);
+            double h = g / 12.5;
+            Console.WriteLine(h);
+
+            Console.WriteLine("Enter another one or two digit number and hit enter.");
+            string ansFour = Console.ReadLine();
+            int i = Convert.ToInt32(ansFour);
+            bool j = i > 50;
+            Console.WriteLine(j);
+
+            Console.WriteLine("Enter another one or two digit number and hit enter.");
+            string ansFive = Console.ReadLine();
+            int k = Convert.ToInt32(ansFour);
+            int l = k % 7;
+            Console.WriteLine(l);
+            Console.ReadLine();
+
+            Console.WriteLine("Hit enter to end this awesome game.");
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
---
+++ incomeCamparison/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace incomeCamparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program.");
+            Console.WriteLine("Enter the details for Person 1:");
+            Console.WriteLine("Hourly Rate: ");
+            string p1Rate = Console.ReadLine();
+            Console.WriteLine("Hours Worked: ");
+            string p1Hours = Console.ReadLine();
+
+            Console.WriteLine("Enter the details for Person 2:");
+            Console.WriteLine("Hourly Rate: ");
+            string p2Rate = Console.ReadLine();
+            Console.WriteLine("Hours Worked: ");
+            string p2Hours = Console.ReadLine();
+
+            int a = Convert.ToInt32(p1Rate);
+            int b = Convert.ToInt32(p1Hours);
+            int c = a * b * 52;
+
+            int d = Convert.ToInt32(p2Rate);
+            int e = Convert.ToInt32(p2Hours);
+            int f = d * e * 52;
+            
+            bool g = c > f;
+
+            Console.WriteLine("Annual salary of Person 1:" + c);
+
+            Console.WriteLine("Annual Salary of Person 2:" + f);
+
+            Console.WriteLine("Does Person 1 make more than Person 2? " + g);
+            Console.ReadLine();
+        }
+    }
+}
