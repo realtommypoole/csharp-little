d99197c050ad367deab41169f8aaed0725524b72
realtommypoole <65101524+realtommypoole@users.noreply.github.com>
2020-06-30 17:51:24 -0700

    ShippingAssignment

---
+++ BranchingExample/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentTemperature = 70;
+            int roomTemperature = 70;
+
+            Console.WriteLine("Hi, what is your name!");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hi, " + name + ", what is the temperature where you are?");
+            int currentTemperature = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is your favorite number?"); 
+                int favNum = Convert.ToInt32(Console.ReadLine());
+
+            string result = favNum == 12 ? "you have an awesome favorite number" : "you do not have an awesome favorite number";
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+
+            if (currentTemperature == roomTemperature)
+            {
+                Console.WriteLine("It is exactly room temperature");
+
+            }
+            else if (currentTemperature < roomTemperature)
+            {
+                
+            }
+            else
+            {
+                Console.
+            }
+
+            string comparisonResult = currentTemperature == roomTemperature ? "It is room temp" : "It is not room temp";
+            Console.WriteLine(comparisonResult);
+            Console.ReadLine();
+
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("It is exactly room temperature!");
+            //}
+            //else if (currentTemperature > roomTemperature)
+            //{
+            //    Console.WriteLine("IT is warmer than room temperature!");
+            //}
+            //else if (currentTemperature < roomTemperature)
+            //{
+            //    Console.WriteLine("Room temperature is greater than current temeperature.")
+            //}
+            //else
+            //{
+            //    Console.WriteLine("It is not exaclty room temperature.")
+            //}
+        }
+    }
+}
---
+++ PackageShippingApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageShippingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Package Express. Please follow the instructions below.");
+            Console.WriteLine("Enter package weight");
+            int pWeight = Convert.ToInt32(Console.ReadLine()); 
+            
+            if (pWeight > 50)
+            {
+                Console.WriteLine("Package is too heavy to ship. Try a different package.");
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("Enter package width");
+            int pWidth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter package height");
+            int pHeight = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter package length");
+            int pLength = Convert.ToInt32(Console.ReadLine());
+
+            if ((pWidth + pHeight + pLength) > 50)
+            {
+                Console.WriteLine("Package is too big to be shipped via Package Express");
+                Console.ReadLine(); 
+            }
+            else
+            {
+                double quote = pWidth * pHeight * pLength * pWeight/100.00;
+                Console.WriteLine("The total cost to ship is: $" + quote);
+                Console.ReadLine();
+            }
+        }
+    }
+}
