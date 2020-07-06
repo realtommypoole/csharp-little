f121d039d3be05c09c85e23c30b16420622263f2
realtommypoole <65101524+realtommypoole@users.noreply.github.com>
2020-07-06 06:07:12 -0700

    TryCatch

---
+++ DivideList/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivideList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> numList = new List<int>() { 1, 2, 6, 658 };
+            Console.WriteLine("Pick a number.");
+            string numberOne = Console.ReadLine();
+
+            try
+            {
+                foreach (int number in numList)
+                {
+                    int numberOner = Convert.ToInt32(numberOne);
+                    Console.WriteLine(number / numberOner);
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            finally
+            {
+                Console.WriteLine("The program has emerged from the try/catch block");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
