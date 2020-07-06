66f9ba4f370b106d0d746bff74f36ffbe2c48553
realtommypoole <65101524+realtommypoole@users.noreply.github.com>
2020-07-06 09:47:02 -0700

    ClassAppThreeMethods

---
+++ ClassApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Which number do you want to do math on? Type: (2, 4, or 6):");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            MyNumber numOne = new MyNumber();
+            numOne.Num = userNum;
+
+            numOne.MyAdder();
+            numOne.MySubber();
+            numOne.MyMulti();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ClassApp/ThreeMethods.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassApp
+{
+    public class MyNumber
+    {
+        public void MyAdder()
+        {
+            Console.WriteLine(Num + 3);
+        }
+
+        public void MySubber()
+        { 
+            Console.WriteLine(Num - 3);
+        }
+        
+        public void MyMulti()
+        {
+            Console.WriteLine(Num * 3);
+        }
+
+        public int Num { get; set; }
+    }
+}
