d4b73d5ec89ff813d83a7bde7f927f3f5e26eb0a
realtommypoole <65101524+realtommypoole@users.noreply.github.com>
2020-07-06 10:57:00 -0700

    MoreMethodsFromClasses

---
+++ MoreClass/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreClass
+{
+    public class Class1
+    {
+        
+        public int MyMe(int Num)
+        {
+            return Num + 3;
+        }
+
+        public double MyMe(double Num)
+        {
+            return Convert.ToInt32(Num + 10.1);
+        }
+
+        public int MyMe(string Num)
+        {
+            int x = Convert.ToInt32(Num);
+            return x + 23;
+        }
+
+        public int Num { get; set; }
+
+    }
+}
---
+++ MoreClass/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ////METHOD ONE
+            //Class1 numOne = new Class1();
+            //int x = 5;
+
+            ////METHOD TWO
+            //Class1 numOne = new Class1();
+            //double x = 4.3;
+
+            //METHOD THREE
+            Class1 numOne = new Class1();
+            string x = "3";
+
+            Console.WriteLine(numOne.MyMe(x));
+            Console.ReadLine();
+        }
+    }
+}
