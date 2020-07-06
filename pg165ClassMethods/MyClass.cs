9773bbc1ab9c51edff7bf40717ceb2ec45a8c5fd
realtommypoole <65101524+realtommypoole@users.noreply.github.com>
2020-07-06 11:31:29 -0700

    pg165assignment

---
+++ pg165ClassMethods/MyClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg165ClassMethods
+{
+    public class MyClass
+    {
+       
+        public int AddNumber(int hours, int speed = 2)
+        {
+            return hours/speed;
+        }
+
+        public double MyMe(double Num)
+        {
+            return Convert.ToInt32(Num + 10.1);
+        }
+
+        public int MyMe(string Num)
+        {
+            int x = Convert.ToInt32(Num);
+            return x + 23;
+        }
+
+        public int Num { get; set; }
+
+        
+    }
+}
---
+++ pg165ClassMethods/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg165ClassMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyClass Number = new MyClass();
+
+            Console.WriteLine("Enter a number between 1-10:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number between 1-10, or leave blank to use the default value:");
+            string y = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(y))
+            { 
+                int z = Number.AddNumber(x);
+                Console.WriteLine("Your first number divided by your second number is: " + z);
+
+            }
+            else
+            {
+                int c = Convert.ToInt32(y);
+                int z = Number.AddNumber(x, c);
+                Console.WriteLine("Your first number divided by your second number is: " + z);
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
