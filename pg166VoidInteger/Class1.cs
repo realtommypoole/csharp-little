e3ab05c75d7358f198f5cad3d15bcd07a25d2667
realtommypoole <65101524+realtommypoole@users.noreply.github.com>
2020-07-06 11:46:02 -0700

    pg166

---
+++ pg166VoidInteger/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg166VoidInteger
+{
+    public class VoidClass
+    {
+        
+        public void TheseNumbers(int nOne, int nTwo)
+        {
+            int x = nOne * 5;
+            Console.WriteLine(nTwo);
+            
+        }
+
+    }
+}
---
+++ pg166VoidInteger/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg166VoidInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VoidClass varOne = new VoidClass();
+            varOne.TheseNumbers(3, 7);
+            varOne.TheseNumbers(nOne: 7, nTwo: 5);
+            Console.ReadLine();
+        }
+    }
+}
