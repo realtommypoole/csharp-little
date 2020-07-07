280d4b59e9a1bcaed286f2012c3f69ea34289b10
realtommypoole <65101524+realtommypoole@users.noreply.github.com>
2020-07-06 21:20:55 -0700

    203 lambda

--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -11,29 +11,35 @@ namespace TwentyOne
         static void Main(string[] args)
         {
 
-            Card card1 = new Card();
-            Card card2 = card1;
-            card1.Face = Face.Eight;
-            card2.Face = Face.King;
-            
-            
-            //Game game = new TwentyOneGame();
-            //game.Players = new List<Player>();
-            //Player player = new Player();
-            //player.Name = "Jesse";
-            //game += player;
+            Deck deck = new Deck();
 
-           
-        //    Deck deck = new Deck();
-        //    deck.Shuffle(3);
+            //int count = deck.Cards.Count(x => x.Face == Face.Ace);
+            //Console.WriteLine(count);
 
-        //    foreach (Card card in deck.Cards)
-        //    {
-        //        Console.WriteLine(card.Face + " of " + card.Suit);
 
-        //    }
-        //    Console.WriteLine(deck.Cards.Count);
-        //    Console.ReadLine();
+            List<int> numberList = new List<int> { 1, 2, 3, 535, 343, 23 };
+
+            int min = numberList.Min();
+            int sum = numberList.Sum();
+            //int sum = numberList.Where(x => x > 20).Sum();
+            Console.WriteLine(sum);
+
+            //List<Card> newList = deck.Cards.Where(x => x.Face == Face.King).ToList();
+
+
+            //foreach (Card card in newList)
+            //{
+            //    Console.WriteLine(card.Face);
+            //}
+
+
+            //    foreach (Card card in deck.Cards)
+            //    {
+            //        Console.WriteLine(card.Face + " of " + card.Suit);
+
+            //    }
+            //    Console.WriteLine(deck.Cards.Count);
+                Console.ReadLine();
         }
         
     }
